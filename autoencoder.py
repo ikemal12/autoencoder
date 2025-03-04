aa9e47cba8487783a96f0679ca868300901bda17
ikemal12 <iltar2005@gmail.com>
2025-03-04 13:20:58 +0000

    Initialise model and training loop

--- autoencoder.py
+++ autoencoder.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
 
-images = np.load("subset_1.npy")
-print(images.shape)
-
-i = 201
-print(plt.imshow(np.reshape(images[i, :], (150, 225, 3))))
 
 class Autoencoder(nn.Module):
     def __init__(self):
-        
+        super().__init__()
+
         self.encoder = nn.Sequential(
             nn.Linear(150*225, 128),
             nn.ReLU(),
@@ -39,3 +37,26 @@ class Autoencoder(nn.Module):
         encoded = self.encoder(x)
         decoded = self.decode(encoded)
         return decoded
+    
+
+transform = transforms.Compose([transforms.ToTensor()])
+images = np.load("subset_1.npy")
+dataloader = torch.utils.data.DataLoader(images, batch_size=64, shuffle=True)
+
+model = Autoencoder()
+criterion = nn.MSELoss()
+optimiser = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 5
+outputs = []
+for epoch in range(num_epochs):
+    for img in dataloader:
+        recon = model(img)
+        loss = criterion(recon, img)
+
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+    outputs.append((epoch, img, recon))
